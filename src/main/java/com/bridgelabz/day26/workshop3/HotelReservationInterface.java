6d353cdc943b7739e1c8cf7420f4483fdd6f91fa
Arpit <arpitmh23@gmail.com>
2022-01-15 18:34:14 +0530

    Adding Uc6

--- src/main/java/com/bridgelabz/day26/workshop3/HotelReservationInterface.java
+++ src/main/java/com/bridgelabz/day26/workshop3/HotelReservationInterface.java
@@ -1,10 +1,20 @@
 package com.bridgelabz.day26.workshop3;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public interface HotelReservationInterface {
-    public void addHotel(String lakewood, int i, int i1, int i2);
+    public void addHotel(String hotelName, int rating, double weekdayRegularCustomerCost, double weekendRegularCustomerCost);
+
+    void addHotel(String lakewood, int i, int i1, int i2);
+
     public int getHotelListSize();
+
     public void printHotelList();
+
     public ArrayList<Hotel> getHotelList();
+
+    public ArrayList<Hotel> getCheapestHotel(LocalDate startDate, LocalDate endDate);
+
+    public Hotel getCheapestBestRatedHotel(LocalDate startDate, LocalDate endDate);
 }
